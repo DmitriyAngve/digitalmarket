d42c2fa929488704b7f0f10799aa90f7991c632b
DmitriyAngve <kirirch129@gmail.com>
2023-12-25 14:13:56 +0300

    #

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,27 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
- 
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+// Change to local currency
+export function formatPrice(
+  price: number | string,
+  options: {
+    currency?: "USD" | "EUR" | "GBP" | "BDT" | "RUB";
+    notation?: Intl.NumberFormatOptions["notation"];
+  } = {}
+) {
+  const { currency = "USD", notation = "compact" } = options;
+
+  // Преобразование строки в число, если "price" оказался строкой
+  const numericPrice = typeof price === "string" ? parseFloat(price) : price;
+
+  return new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: currency,
+    notation: notation,
+    maximumFractionDigits: 2,
+  }).format(numericPrice);
 }
